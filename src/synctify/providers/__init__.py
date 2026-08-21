96403b89556b4227b97ef4b4dcacb22e937ace65
Angelo David <90495108+notsonata@users.noreply.github.com>
2026-08-21 22:22:26 +0800

    feat: export qobuz provider

--- src/synctify/providers/__init__.py
+++ src/synctify/providers/__init__.py
@@ -1,3 +1,16 @@
 from .base import AcquiredTrack, AcquisitionProvider
+from .qobuz import (
+    QobuzDLConfig,
+    QobuzDLDownloadError,
+    QobuzDLProvider,
+    QobuzDLUnavailableError,
+)
 
-__all__ = ["AcquiredTrack", "AcquisitionProvider"]
+__all__ = [
+    "AcquiredTrack",
+    "AcquisitionProvider",
+    "QobuzDLConfig",
+    "QobuzDLDownloadError",
+    "QobuzDLProvider",
+    "QobuzDLUnavailableError",
+]
